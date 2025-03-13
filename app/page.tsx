2a3948a4059efa83f5f4d37c3b9183c94ac0b36d
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-03-13 14:24:31 +0545

    Home page layout with components pages render

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+import {
+  Track,
+  Timeline,
+  Sponsor,
+  Prizes,
+  KeyInfo,
+  Judges,
+  Heroland,
+  Footer,
+  FAQ,
+} from "./pages";
 import HeroPage from "./pages/HeroPage";
 
 export default function Home() {
   return (
     <div>
       <HeroPage />
+      <Heroland />
+      <KeyInfo />
+      <Timeline />
+      <Track />
+      <Judges />
+      <Prizes />
+      <Sponsor />
+      <FAQ />
+      <Footer />
     </div>
   );
 }
