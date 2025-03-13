5f0196282cc1893dcedc3f18df677b80240cdc03
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-03-13 14:26:09 +0545

    barrel file for pages

---
+++ app/pages/index.ts
@@ -0,0 +1,25 @@
+import FAQ from "./FAQ";
+import Footer from "./Footer";
+import Heroland from "./Heroland";
+import Judges from "./Judges";
+import KeyInfo from "./KeyInfo";
+import Prizes from "./Prizes";
+import Sponsor from "./Sponsor";
+import Timeline from "./Timeline";
+import Track from "./Track";
+
+
+export {
+    Track,
+    Timeline,
+    Sponsor,
+    Prizes,
+    KeyInfo,
+    Judges,
+
+    Heroland,
+
+    Footer,
+
+    FAQ
+}
