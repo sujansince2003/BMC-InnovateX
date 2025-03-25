65422a9b680da0b37ccaf3d99458be30e35fa71d
im-bipo <bipinkhatri.ram@gmail.com>
2025-03-25 19:33:27 +0545

    style: faq max width

--- app/component/FAQ.tsx
+++ app/component/FAQ.tsx
@@ -36,12 +36,12 @@ const faqData = [
 
 export default function FAQ() {
   return (
-    <section className="py-16 bg-muted/50">
-      <div className="container mx-auto px-4">
+    <section className="pb-16 bg-muted/50">
+      <div className="mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-12">
           Frequently Asked Questions
         </h2>
-        <div className="max-w-3xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <Accordion type="single" collapsible className="w-full">
             {faqData.map((faq, index) => (
               <AccordionItem key={index} value={`item-${index}`}>
--- components/custom/Dots.tsx
+++ components/custom/Dots.tsx
@@ -1,35 +1,11 @@
-"use client";
-import React, { useState, useEffect } from "react";
-
 function Dots({ className }: Readonly<{ className: string }>) {
   return <div className={`h-1.5 w-1.5 rounded-full bg-white ${className}`} />;
 }
 
 function LeftDots() {
-  const [dotCount, setDotCount] = useState(18);
-
-  useEffect(() => {
-    let isIncreasing = false;
-    const interval = setInterval(() => {
-      setDotCount((prevCount) => {
-        if (isIncreasing) {
-          if (prevCount < 18) return prevCount + 1;
-          isIncreasing = false;
-          return prevCount - 1;
-        } else {
-          if (prevCount > 0) return prevCount - 1;
-          isIncreasing = true;
-          return prevCount + 1;
-        }
-      });
-    }, 150);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="grid w-10 grid-cols-3 gap-x-5 gap-y-3">
-      {Array.from({ length: dotCount }).map((_, index) => (
+      {Array.from({ length: 18 }).map((_, index) => (
         <Dots key={index} className="transition-opacity opacity-100" />
       ))}
     </div>
@@ -37,30 +13,9 @@ function LeftDots() {
 }
 
 function RightDots() {
-  const [dotCount, setDotCount] = useState(18);
-
-  useEffect(() => {
-    let isIncreasing = false;
-    const interval = setInterval(() => {
-      setDotCount((prevCount) => {
-        if (isIncreasing) {
-          if (prevCount < 18) return prevCount + 1;
-          isIncreasing = false;
-          return prevCount - 1;
-        } else {
-          if (prevCount > 0) return prevCount - 1;
-          isIncreasing = true;
-          return prevCount + 1;
-        }
-      });
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="grid w-10 grid-cols-5 gap-x-6 gap-y-3">
-      {Array.from({ length: dotCount }).map((_, index) => (
+      {Array.from({ length: 18 }).map((_, index) => (
         <Dots key={index} className="transition-opacity opacity-100" />
       ))}
     </div>
