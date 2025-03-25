5f5f6570a0c28bff6e3bae0b0602b35bbcc5e391
im-bipo <bipinkhatri.ram@gmail.com>
2025-03-25 16:50:16 +0545

    style: add animation in X of BMC InnovateX

--- app/component/HeroLander.tsx
+++ app/component/HeroLander.tsx
@@ -3,16 +3,37 @@ import Image from "next/image";
 
 const HeroLander = () => {
   return (
-    <div className="h-[45rem] lg:h-screen bg-[#008FAD] relative overflow-hidden">
+    <div className="h-[45rem] min-h-[40rem] lg:h-screen bg-[#008FAD] relative overflow-hidden">
       <div className="z-10 flex flex-col gap-14 md:gap-12 items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
         <div className="flex flex-col gap-5 md:gap-6 items-center justify-center text-center">
           <h1 className="text-xl md:text-3xl lg:text-[30px] text-white font-medium">
             NATIONAL LEVEL HACKATHON
           </h1>
-          <h1 className="text-5xl md:text-5xl lg:text-[90px] font-extrabold text-white">
+          {/* <h1 className="text-5xl md:text-5xl lg:text-[90px] font-extrabold text-white">
             BMC InnovateX
-          </h1>
-
+          </h1> */}
+          <div className="w-[100vw]">
+            <svg
+              className="h-[4rem] md:h-[unset] svg-x w-full flex justify-center items-center"
+              width="100%"
+              height="100%"
+            >
+              <pattern className="svg-stuff"></pattern>
+              <text
+                x="50%"
+                y="70%"
+                color="white"
+                textAnchor="middle"
+                fontSize="30"
+                className="text-[44px] md:text-[70px] lg:text-[90px] py-20 font-extrabold text-white"
+                fill="white"
+                dominantBaseline="end"
+              >
+                BMC Innovate
+                <tspan className="animated-text">X</tspan>{" "}
+              </text>
+            </svg>
+          </div>
           <h1 className="text-white text-xl md:text-3xl lg:text-[30px] font-medium text-center">
             Think Big, Innovate Bigger
           </h1>
